4d832013b47f6d0d407145e61dbbfa89df7e9972
Vishant <vishantkumar755@gmail.com>
2025-10-25 22:57:29 +0530

    Fix login redirect, restore session from localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
 import Login from './components/Auth/Login'
 import AdminDashboard from './components/dashBoard/AdminDashboard'
 import EmployeeDashboard from './components/dashBoard/EmployeeDashboard'
-import type { RootState } from './store/store'
+import type { AppDispatch, RootState } from './store/store'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { loginSuccess } from './store/userSlice'
+import { useEffect } from 'react'
 
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
   const { role } = useSelector((state: RootState) => state.user);
+
+  useEffect(() => {
+    const savedUserData = localStorage.getItem('userData');
+    if (savedUserData) {
+      try {
+        const userData = JSON.parse(savedUserData);
+        dispatch(loginSuccess(userData)); 
+      } catch (error) {
+        console.error('Failed to restore user data:', error);
+        localStorage.removeItem('userData');
+      }
+    }
+  }, [dispatch]);
+
 
   return (
     <BrowserRouter>
